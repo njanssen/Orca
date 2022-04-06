03472e318118b4ea9c44c541fcad6ab982d3c99d
Niels Janssen <niels@vliegwerk.com>
2022-04-06 21:31:05 +0200

    Initial working upgrade to Electron 18, stillr requires regression testing for Electron features (e.g. menu options)

--- desktop/sources/scripts/lib/acels.js
+++ desktop/sources/scripts/lib/acels.js
@@ -94,7 +94,7 @@ function Acels (client) {
   // Electron specifics
 
   this.inject = (name = 'Untitled') => {
-    const app = require('electron').remote.app
+    const app = require("@electron/remote").app;
     const injection = []
 
     injection.push({
